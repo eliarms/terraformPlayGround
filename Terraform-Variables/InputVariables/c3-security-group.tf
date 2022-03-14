fe4d4c9a10bac08a4bed9441a49615090531eb60
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2022-03-14 00:01:16 -0400

    Terraform basics variables

---
+++ Terraform-Variables/InputVariables/c3-security-group.tf
@@ -0,0 +1,53 @@
+# Create Security Group - SSH Traffic
+resource "aws_security_group" "vpc-ssh-sg" {
+
+  name        = "vpc-dev-ssh-sg"
+  description = "Dev vpc ssh sg"
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    description = "Allow all IP and ports ourbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group - WEB Traffic
+resource "aws_security_group" "vpc-web-sg" {
+
+  name        = "vpc-dev-web-sg"
+  description = "Dev vpc web sg"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    description = "Allow all IP and ports ourbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
